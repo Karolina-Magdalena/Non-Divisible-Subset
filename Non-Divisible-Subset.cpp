722a1227ed4209e20bcd1c7e5851cb5629ecdaa0
Karolina-Magdalena <mccarol@interia.pl>
2020-11-16 11:23:41 +0100

    Add new files

---
+++ Non-Divisible-Subset.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+int nonDivisibleSubset(int k, vector<int> s) {
+int m = 0;
+int modulo[k] = {0};
+for (int i=0;i<s.size();i++)
+{
+    m=s[i]%k;
+    modulo[m]++;
+}
+int r=0;
+for(int i=1;i<=k/2;i++)
+{
+    if(i!=k-i)
+        {
+        r+=max(modulo[i],modulo[k-i]);
+        }
+    else {r+=1;}
+}
+if(modulo[0]!=0) {r+=1;}
+return r;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int k = stoi(first_multiple_input[1]);
+
+    string s_temp_temp;
+    getline(cin, s_temp_temp);
+
+    vector<string> s_temp = split(rtrim(s_temp_temp));
+
+    vector<int> s(n);
+
+    for (int i = 0; i < n; i++) {
+        int s_item = stoi(s_temp[i]);
+
+        s[i] = s_item;
+    }
+
+    int result = nonDivisibleSubset(k, s);
+
+    cout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Non-Divisible-Subset
+
+For any number K, the sum of 2 values (A & B) is evenly divisible by K if the sum of the remainders of A/K + B/K is K.
+
+I Inititalized k size array with 0 to store frequency of remainders for all m = s[i]%k;
+
+why k=size? because if we divide by k, the remainder are always less than k;
+
+how to store the frequency?
+
+for (int i=0;i<s.size();i++)
+{
+    m=s[i]%k;
+    modulo[m]++;
+}
+
+How to compare two values:
+
+max(modulo[i],modulo[k-i]);
+
+
